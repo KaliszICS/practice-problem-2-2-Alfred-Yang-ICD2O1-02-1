142219fa364b90cb7291131395fbd2273135fc50
Alfred-Yang-ICD2O1-02 <alfred.yang@student.tdsb.on.ca>
2026-03-11 15:43:13 +0000

    birnsgiosbenga

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,28 @@
+/**
+ * File: PracticeProblem More Functions
+ * Author: Alfred Yang
+ * Date Created: Mar 11, 2026
+ * Date Last Modified: Mar 11, 2026
+ */
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static void main(String args[]) {}
+		
+	public static boolean isEven(int num) {
+		return (num%2 == 0);}
+
+	public static boolean isOdd(int num){
+		return (num%2 !=0);}
+
+	public static boolean isPositive(int num){
+		return (num > 0);}
+
+	public static boolean isNegative(int num){
+		return (num < 0);}
+
+	public static int combinedLength(String bug, String tug){
+		return bug.length() + tug.length();}
+
+
 
 	}
-
-}
